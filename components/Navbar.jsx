e626497dbdcde3667acc2dd637e6e4e11342f5f4
Horacio Abitu <ahabitu@gmail.com>
2023-12-20 22:37:25 -0300

    dark and light theme width toggle 2

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from "react";
-import Image from 'next/image';
+import Image from "next/legacy/image";
 import styles from './Navbar.module.css';
 import Link from "next/link";
 import { FiMoon } from 'react-icons/fi';
